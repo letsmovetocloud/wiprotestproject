8bdd1cd4d6756b4ff00fa9b35f153910ea2efa4e
letsmovetocloud <88219893+letsmovetocloud@users.noreply.github.com>
2024-02-21 07:55:42 +0530

    Update to gcp

--- ec2.tf
+++ ec2.tf
@@ -2,14 +2,14 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source  = "hashicorp/gcp"
       
     }
   }
 }
 
 # Provider Block
-provider "azure" {
+provider "gcp" {
   profile = "default" 
   region  = "us-east-1"
 }
